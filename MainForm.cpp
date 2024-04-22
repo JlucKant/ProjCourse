747cf764234be24cc64469ba800642de844c520f
Mihail Dementev <kantyt3@gmail.com>
2024-04-23 01:53:00 +0300

    Remake

--- MainForm.cpp
+++ MainForm.cpp
@@ -12,30 +12,130 @@ int main(array<String^>^ args) {
 	Application::Run(% form);
 }
 
-System::Void ProjCourse::MainForm::button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	this->chart1->Series[0]->Points->Clear();
-	float t[1000], Uvx[1000], Uvix[1000];
-	int n = 1000;
-	float tn = 15, tk = 100, t1 = 30, t2 = 60;
-	float a = 50, b = 0.07, c = 0.1;
+//searching maximum
+float funcmax(int n, float* U) {
+	float Umax = 0;
+	for (int i = 0; i < n; i++) {
+		if (U[i] > Umax) Umax = U[i];
+	}
+	return Umax;
+}
+
+
+//count parameter 
+float parameter(float* U, float Umax, int n, float dt) {
+	float U1 = U[n] + 0.9 * (Umax - U[n]);
+	float U2 = U[n] + 0.1 * (Umax - U[n]);
+	float dlit = 0;
+	for (int i = 0; i < n + 1; i++)
+		if (U[i] < U1 && U[i] > U2 && U[i + 1] < U[i]) dlit += dt;
+	return dlit;
+}
+
+//making the array of time
+void timeArray(float* t, int n) {
+	float tn = 15, tk = 100;
 	float dt = (tk - tn) / (n - 1);
-	//making the array of time
 	for (int i = 0; i < n; i++) {
 		t[i] = tn + i * dt;
 	}
+}
 
-	//making array of Uvx
+//making array of Uvx
+void UvxArray(float* Uvx, float* t, int n) {
+	float tn = 15, tk = 100, t1 = 30, t2 = 60;
+	float a = 50, b = 0.07, c = 0.1;
 	for (int i = 0; i < n; i++) {
 		if (t[i] <= t1) Uvx[i] = a * (1 - exp(-b * (t[i] - tn)));
 		else if (t1 < t[i] && t[i] <= t2) Uvx[i] = a * (1 - exp(-b * (t1 - tn)));
 		else Uvx[i] = a * (1 - exp(-b * (t1 - tn))) * exp(-c * (t[i] - t2));
-		this->chart1->Series[0]->Points->AddXY(t[i],Uvx[i]);
-	}
 
-	//making array of Uvix
+	}
+}
+
+//making array of Uvix
+void UvixArray(float* Uvix, float* Uvx, int n) {
 	for (int i = 0; i < n; i++) {
 		if (Uvx[i] < 15) Uvix[i] = (7.f / 3.f) * Uvx[i];
 		else Uvix[i] = 35;
+
+	}
+}
+
+
+
+//calculating fault
+void fault(float tk, float tn, int n, int massive,float fault_) {
+	float fault=fault_;
+	float p = 1;
+	float param = pow(2.f, 20.f);
+
+	while (p >= fault) {
+		float* Uvx = new float[n];
+		float* Uvix = new float[n];
+		float* t = new float[n];
+		float dt = (tk - tn) / (n - 1);
+		timeArray(t, n);
+		float param1;
+		if (massive == 1) {
+			UvxArray(Uvx, t, n);
+			param1 = parameter(Uvx, funcmax(n, Uvx), n - 1, dt);
+		}
+		else {
+			UvxArray(Uvx, t, n);
+			UvixArray(Uvix, Uvx, n);
+			param1 = parameter(Uvx, funcmax(n, Uvix), n - 1, dt);
+		}
+
+		p = fabs(param - param1) / param1;
+
+		param = param1;
+		n *= 2;
+	}
+}
+System::Void ProjCourse::MainForm::button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->chart1->Series[0]->Points->Clear();
+	this->chart2->Series[0]->Points->Clear();
+	int n;
+	float fault_;
+	try {
+		n = Int32::Parse(textBox1->Text);
+		if (n < 2 || n >1000) e;
+		else {
+			fault_ = float::Parse(textBox5->Text);
+			float* Uvx = new float[n];
+			float* Uvix = new float[n];
+			float* t = new float[n];
+			float tn = 15, tk = 100;
+
+			timeArray(t, n);
+			UvxArray(Uvx, t, n);
+			UvixArray(Uvix, Uvx, n);
+			for (int i = 0; i < n; i++) {
+				this->chart1->Series[0]->Points->AddXY(t[i], Uvx[i]);
+				this->chart2->Series[0]->Points->AddXY(t[i], Uvix[i]);
+				this->textBox2->Text += t[i].ToString() + "\t" + Uvx[i].ToString() + "\t" + Uvix[i].ToString()+"\n";
+			}
+
+			fault(tk, tn, n, 1, fault_);
+			fault(tk, tn, n, 2, fault_);
+		}
+	}
+	catch (FormatException^ e) {
+		MessageBox::Show("Некорректный ввод", "Ошибка", MessageBoxButtons::OK, MessageBoxIcon::Warning);
 	}
 	
+	
 }
+//System::Void ProjCourse::MainForm::textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+//	try {
+//		int a = Int32::Parse(textBox1->Text);
+//	}
+//	catch (FormatException^) {
+//		textBox1->Text = "";
+//	}
+//}
+//
+//System::Void ProjCourse::MainForm::textBox5_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+//
+//}
--- MainForm.h
+++ MainForm.h
@@ -36,6 +36,16 @@ namespace ProjCourse {
 		}
 	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox5;
+
+
 	protected:
 
 	private:
@@ -54,9 +64,21 @@ namespace ProjCourse {
 			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
 			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
 			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea2 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
 			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->chart2 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// chart1
@@ -65,7 +87,7 @@ namespace ProjCourse {
 			this->chart1->ChartAreas->Add(chartArea1);
 			legend1->Name = L"Legend1";
 			this->chart1->Legends->Add(legend1);
-			this->chart1->Location = System::Drawing::Point(12, 12);
+			this->chart1->Location = System::Drawing::Point(26, 12);
 			this->chart1->Name = L"chart1";
 			series1->BorderColor = System::Drawing::Color::Transparent;
 			series1->BorderWidth = 3;
@@ -75,34 +97,135 @@ namespace ProjCourse {
 			series1->Legend = L"Legend1";
 			series1->Name = L"Входное напряжение";
 			this->chart1->Series->Add(series1);
-			this->chart1->Size = System::Drawing::Size(859, 317);
+			this->chart1->Size = System::Drawing::Size(501, 203);
 			this->chart1->TabIndex = 1;
 			this->chart1->Text = L"chart1";
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(721, 335);
+			this->button1->Location = System::Drawing::Point(788, 382);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(150, 44);
+			this->button1->Size = System::Drawing::Size(191, 42);
 			this->button1->TabIndex = 2;
 			this->button1->Text = L"Рассчитать";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// chart2
+			// 
+			chartArea2->Name = L"ChartArea1";
+			this->chart2->ChartAreas->Add(chartArea2);
+			legend2->Name = L"Legend1";
+			this->chart2->Legends->Add(legend2);
+			this->chart2->Location = System::Drawing::Point(26, 221);
+			this->chart2->Name = L"chart2";
+			series2->BorderColor = System::Drawing::Color::Transparent;
+			series2->BorderWidth = 3;
+			series2->ChartArea = L"ChartArea1";
+			series2->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Line;
+			series2->Color = System::Drawing::Color::Black;
+			series2->Legend = L"Legend1";
+			series2->Name = L"Выходное напряжение";
+			this->chart2->Series->Add(series2);
+			this->chart2->Size = System::Drawing::Size(501, 203);
+			this->chart2->TabIndex = 3;
+			this->chart2->Text = L"chart2";
+			this->chart2->Click += gcnew System::EventHandler(this, &MainForm::chart2_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(788, 243);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(191, 22);
+			this->textBox1->TabIndex = 4;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(817, 224);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(140, 16);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"Параметр (1<n<1000)";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(555, 12);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(216, 203);
+			this->textBox2->TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(777, 12);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(216, 203);
+			this->textBox3->TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(555, 221);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->ReadOnly = true;
+			this->textBox4->Size = System::Drawing::Size(216, 203);
+			this->textBox4->TabIndex = 8;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(817, 280);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(131, 16);
+			this->label2->TabIndex = 10;
+			this->label2->Text = L"Погрешность (n < 1)";
+			this->label2->Click += gcnew System::EventHandler(this, &MainForm::label2_Click);
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(788, 299);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(191, 22);
+			this->textBox5->TabIndex = 9;
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1001, 460);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox5);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->chart2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->chart1);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart2))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
-	};
+	private: System::Void chart2_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+
+
+
+
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+
+
+};
 }
